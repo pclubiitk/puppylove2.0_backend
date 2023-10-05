66ce5da347d124b87d3fe438a8ac1d3c9086e7ee
Gill-Singh-A <amansg22@iitk.ac.in>
2023-10-05 17:47:37 +0530

    Added Test Suite

---
+++ test_suite/test_suite.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+import requests, json, hashlib, base64
+from datetime import date
+from OpenSSL import crypto
+from Crypto import Random
+from Crypto.Cipher import AES
+from time import strftime, localtime
+from colorama import Fore, Back, Style
+
+status_color = {
+    '+': Fore.GREEN,
+    '-': Fore.RED,
+    '*': Fore.YELLOW,
+    ':': Fore.CYAN,
+    ' ': Fore.WHITE
+}
+
+def display(status, data, start='', end='\n'):
+    print(f"{start}{status_color[status]}[{status}] {Fore.BLUE}[{date.today()} {strftime('%H:%M:%S', localtime())}] {status_color[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}", end=end)
+
+if __name__ == "__main__":
+    pass
