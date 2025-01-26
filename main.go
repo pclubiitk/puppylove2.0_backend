8ca99786f48ff1f3c27bd2d5a61577447b0b5f0a
aayush01x <aayusha23@iitk.ac.in>
2025-01-27 00:34:47 +0530

    Implement rate limiting using Redis

--- main.go
+++ main.go
@@ -13,8 +13,17 @@ import (
 	"github.com/pclubiitk/puppylove2.0_backend/router"
 	"github.com/pclubiitk/puppylove2.0_backend/utils"
 	"github.com/pclubiitk/puppylove2.0_backend/redisclient"
+	"github.com/JGLTechnologies/gin-rate-limit"
+	"time"
+	"strconv"
 )
+func keyFunc(c *gin.Context) string {
+	return c.ClientIP()
+}
 
+func errorHandler(c *gin.Context, info ratelimit.Info) {
+	c.String(429, "Too many requests. Try again in "+time.Until(info.ResetTime).String())
+}
 func main() {
 	// Load environment variables from .env file
 	if err := godotenv.Load(); err != nil {
@@ -25,7 +34,22 @@ func main() {
 	redisclient.InitRedis()
 	utils.Randinit()
 	store := cookie.NewStore([]byte(CfgAdminPass))
+	rateLimitStr := os.Getenv("RATE_LIMIT")
+	rateLimit, err := strconv.ParseUint(rateLimitStr, 10, 32)
+	if err != nil {
+		panic("Invalid RATE_LIMIT value in .env file")
+	}
+	rateLimitStore := ratelimit.RedisStore(&ratelimit.RedisOptions{
+			RedisClient: redisclient.RedisClient, 
+			Rate:        time.Second,       
+			Limit:       uint(rateLimit),
+		})
+	rateLimitMiddleware := ratelimit.RateLimiter(rateLimitStore, &ratelimit.Options{
+		ErrorHandler: errorHandler,
+		KeyFunc:      keyFunc,
+	})
 	r := gin.Default()
+	r.Use(rateLimitMiddleware)
 	// Local Testing Frontend Running on localhost:3000
 	// r.Use(cors.New(cors.Config{AllowCredentials: true, AllowOrigins: []string{"http://localhost:3000"}, AllowHeaders: []string{"content-type"}}))
 	// Allow all origins
